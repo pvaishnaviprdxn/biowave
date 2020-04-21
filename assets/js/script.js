1e5ee5f06452e67b2011216a2d3e476e031f9bca
vaishnavi <vaishnavip@prdxn.com>
2020-04-21 16:01:47 +0530

    added all conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,4 +12,38 @@ function learnMore(idname){
         show2.classList.add('load');
         lm2.classList.add('readmore');
     }
+    else if(idname == 'f3p1'){
+        var show3 = document.getElementById("f3pm");
+        var lm3 = document.getElementById("more1");
+        show3.classList.add('load');
+        lm3.classList.add('readmore');
+    }
+    else if(idname == 'f6p1'){
+        var show4 = document.getElementById("f6pm");
+        var lm4 = document.getElementById("more2");
+        show4.classList.add('load');
+        lm4.classList.add('readmore');
+    }
+    else if(idname == 'f7p1'){
+        var show5 = document.getElementById("f7pm");
+        var lm5 = document.getElementById("more3");
+        show5.classList.add('load');
+        lm5.classList.add('readmore');
+    }
+    else if(idname == 'p2p1'){
+        var show6 = document.getElementById("pro1p");
+        var lm6 = document.getElementById("pro1");
+        show6.classList.add('load');
+        lm6.classList.add('readmore');
+    }
+    else if(idname == 'p3p1'){
+        var show7 = document.getElementById("pro3p");
+        var lm7 = document.getElementById("pro3");
+        show7.classList.add('load');
+        lm7.classList.add('readmore');
+    }
+    else{
+        return;
+    }
+
 }
